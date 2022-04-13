ee2711a1dddf842cb95ff1dfabafa795266a89c4
DANIEL ASSIS GOMES <daniel.agomes7@senacsp.edu.br>
2022-04-13 14:43:40 -0300

    first commit

---
+++ Exercicios/ModelExercicios.cs
@@ -0,0 +1,56 @@
+﻿namespace Exercicios
+{
+    class ModelExercicios
+    {
+        //Declaração de váriaveis
+        private int A;
+        private int B;
+        private int C;
+
+        //Declarar o construtor
+        public ModelExercicios()
+        {
+            ConsultarA = 10;
+            ConsultarB = 20;
+            ConsultarC = 0;
+        }
+
+        public int ConsultarA
+        {
+            get
+            {
+                return A;
+            }
+            set
+            {
+                this.A = value;
+            }
+        }
+
+        public int ConsultarB
+        {
+            get
+            {
+                return B;
+            }
+            set
+            {
+                this.B = value;
+            }
+        }
+
+        public int ConsultarC
+        {
+            get
+            {
+                return C;
+            }
+            set
+            {
+                this.C = value;
+            }
+
+            public double ConsultarA()
+        }
+    }
+}
